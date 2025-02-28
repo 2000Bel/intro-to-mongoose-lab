3bf534b6b9b510e0a8f082efde25b21726784984
2000Bel <belvelizm@gmail.com>
2025-02-28 14:47:01 -0500

    mongoose

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB conectado"))
-  .catch((err) => console.error("Error in the conection to MongoDB:", err));
-
-module.exports = mongoose;
